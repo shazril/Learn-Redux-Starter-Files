9504df4d70025b54aec7cdba2c4f91dfd422b48b
Shazril Shohaimi <minuteman88@gmail.com>
2018-02-18 17:06:50 +0800

    6.a Create action creator - fix typo

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -7,7 +7,7 @@ export function increment(index)  {
 }
 
 // add comment
-export function addComment(postID, author, comment) {
+export function addComment(postId, author, comment) {
     return {
         type: 'ADD_COMMENT',
         postId,
